b5ed83d3cf16924f8f2840f6c08f9e859c6fc1f2
crescentface <c_faze@hotmail.co.uk>
2023-08-17 19:34:07 +0100

    Practise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, remove) {
+    remove = 1;
+    which = 3;
+    const removeFrom = array.splice(which, remove);
+    return array;
 };
 
 // Do not edit below this line
