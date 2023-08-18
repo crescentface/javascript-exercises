15718331252eea374a8c7e4724ee383016534f01
crescentface <c_faze@hotmail.co.uk>
2023-08-18 19:22:41 +0100

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,20 @@
 const sumAll = function(lowerLimit, upperLimit) {
     let finalSum = 0;   
-    if (lowerLimit || upperLimit != parseInt) {
-        return "ERROR"; 
-    } else 
-    if (lowerLimit < 0 || upperLimit < 0) {
-        return "ERROR";
-    } else                                           {
-    for (let i = lowerLimit; i <= upperLimit; i++) { 
-        finalSum += i;
+    if (!Number.isInteger(lowerLimit) || !Number.isInteger(upperLimit)) return "ERROR"; 
+    if (lowerLimit < 0 || upperLimit < 0) return "ERROR";
+    if (lowerLimit > upperLimit) {
+       for (let i = upperLimit; i <= lowerLimit; i++) {
+       finalSum += i;
+       } 
+       } else {                                        
+         for (let i = lowerLimit; i <= upperLimit; i++) { 
+         finalSum += i;
+       }
     }
-}
     return finalSum;
-};
+}
+    
+
 
 // Do not edit below this line
 module.exports = sumAll;
