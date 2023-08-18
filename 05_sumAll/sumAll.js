ae137aa21c63e18275f18c89bd9522f2adf9901b
crescentface <c_faze@hotmail.co.uk>
2023-08-18 14:36:31 +0100

    Produces error if negativenumber or not an integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(lowerLimit, upperLimit) {
+    let finalSum = 0;   
+    if (lowerLimit || upperLimit != parseInt) {
+        return "ERROR"; 
+    } else 
+    if (lowerLimit < 0 || upperLimit < 0) {
+        return "ERROR";
+    } else                                           {
+    for (let i = lowerLimit; i <= upperLimit; i++) { 
+        finalSum += i;
+    }
+}
+    return finalSum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
